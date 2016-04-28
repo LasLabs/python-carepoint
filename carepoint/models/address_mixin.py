44cf4e689c0c88d1edd84f57b7096f2f787a9776
Dave Lasley <dave@laslabs.com>
2016-04-28 10:14:35 -0700

    Fix syntax in ForeignKey on address mixin model

--- carepoint/models/address_mixin.py
+++ carepoint/models/address_mixin.py
@@ -7,17 +7,21 @@ from sqlalchemy import (Column,
                         DateTime,
                         ForeignKey
                         )
+from sqlalchemy.ext.declarative import declared_attr
 
 
 class AddressMixin(object):
     """ This is a mixin for Address Many2Many bindings """
 
-    addr_id = Column(
-        Integer,
-        ForeignKey('csaddr.addr_id'),
-        primary_key=True,
-    )
     priority = Column(Integer)
     addr_type_cn = Column(Integer)
     app_flags = Column(Integer)
     timestmp = Column(DateTime)
+
+    @declared_attr
+    def addr_id(cls):
+        return Column(
+            Integer,
+            ForeignKey('csaddr.addr_id'),
+            primary_key=True,
+        )
