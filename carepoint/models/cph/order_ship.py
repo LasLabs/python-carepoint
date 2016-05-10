fe0cdfdd8c5a212ea0baf46a867939c07a7b1b3d
Dave Lasley <dave@laslabs.com>
2016-05-10 16:13:17 -0700

    Add order ship model to carepoint cph

---
+++ carepoint/models/cph/order_ship.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# © 2015-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from carepoint import Carepoint
+from sqlalchemy import (Column,
+                        Integer,
+                        String,
+                        )
+
+
+class OrderShip(Carepoint.BASE):
+    __tablename__ = 'csom_ship'
+    __dbname__ = 'cph'
+
+    order_id = Column(Integer, primary_key=True)
+    processing_flag = Column(Integer)
+    tracking_code = Column(String)
+    Bill_Name = Column(String)
+    bill_addr1 = Column(String)
+    bill_addr2 = Column(String)
+    bill_addr3 = Column(String)
+    bill_city = Column(String)
+    bill_state_cd = Column(String)
+    bill_zip = Column(String)
+    bill_country_cd = Column(String)
+    ship_name = Column(String)
+    ship_addr1 = Column(String)
+    ship_addr2 = Column(String)
+    ship_addr3 = Column(String)
+    ship_city = Column(String)
+    ship_state_cd = Column(String)
+    ship_zip = Column(String)
+    ship_country_cd = Column(String)
+    ship_phone = Column(String)
+    ship_email = Column(String)
---
+++ carepoint/tests/models/cph/test_order_ship.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+import unittest
+from sqlalchemy.schema import Table
+from carepoint.tests.db.db import DatabaseTest
+from carepoint.models.cph.order_ship import OrderShip
+
+
+class TestModelsCphOrderShip(DatabaseTest):
+
+    def test_table_initialization(self, ):
+        self.assertIsInstance(OrderShip.__table__, Table)
+
+
+if __name__ == '__main__':
+    unittest.main()
