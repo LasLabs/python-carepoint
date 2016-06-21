d5dcfd76e4e0b7646c790885d368a5082c8a813e
Dave Lasley <dave@laslabs.com>
2016-06-21 16:55:52 -0700

    [ADD] Create FDB Monograph models

---
+++ carepoint/models/cph/fdb_pem_moe.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2015-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from carepoint import Carepoint
+from sqlalchemy import (Column,
+                        Integer,
+                        Boolean,
+                        String,
+                        )
+
+
+class FdbPemMoe(Carepoint.BASE):
+    """ Monographs - Text Portion """
+    __tablename__ = 'fdrpemmoe'
+    __dbname__ = 'cph'
+
+    pemono = Column(Integer, primary_key=True)
+    pemono_sn = Column(Integer, primary_key=True)
+    pemtxtei = Column(String)
+    pemtxte = Column(String)
+    pemgndr = Column(String)
+    pemage = Column(String)
+    update_yn = Column(Boolean)
---
+++ carepoint/models/cph/fdb_pem_mogc.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from carepoint import Carepoint
+from sqlalchemy import (Column,
+                        Integer,
+                        Boolean,
+                        ForeignKey,
+                        )
+
+
+class FdbPemMogc(Carepoint.BASE):
+    __tablename__ = 'fdrpemogc'
+    __dbname__ = 'cph'
+
+    gcn_seqno = Column(Integer, primary_key=True)
+    pemono = Column(
+        Integer,
+        ForeignKey('fdrpemmoe.pemono'),
+        primary_key=True,
+    )
+    update_yn = Column(Boolean)
