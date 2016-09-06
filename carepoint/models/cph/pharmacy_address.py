e6d2bbe1c73051965d5e44179ff4d8ccf3771115
Dave Lasley <dave@laslabs.com>
2016-09-06 14:22:59 -0700

    [ADD] Pharmacy & PharmacyAddress models * Add pharmacy (csorg) model * Add pharmacy_address (csorg_addr) model

---
+++ carepoint/models/cph/pharmacy_address.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from carepoint import Carepoint
+from carepoint.models.address_mixin import AddressMixin
+from sqlalchemy import (Column,
+                        Integer,
+                        )
+
+
+class PharmacyAddress(AddressMixin, Carepoint.BASE):
+    __dbname__ = 'cph'
+    __tablename__ = 'csorg_addr'
+    org_id = Column(Integer, primary_key=True)
---
+++ carepoint/tests/models/cph/test_pharmacy.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+import unittest
+from sqlalchemy.schema import Table
+from carepoint.tests.db.db import DatabaseTest
+from carepoint.models.cph.pharmacy import Pharmacy
+
+
+class TestModelsCphPharmacy(DatabaseTest):
+
+    def test_table_initialization(self, ):
+        self.assertIsInstance(Pharmacy.__table__, Table)
+
+
+if __name__ == '__main__':
+    unittest.main()
